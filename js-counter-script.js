36e20d9ddc3e486999b6f98fc0a4f72d96953aef
TetsuoS2 <jimenez12mark@gmail.com>
2017-03-22 09:06:59 +0800

    Update js-counter-script.js

--- js-counter-script.js
+++ js-counter-script.js
@@ -5,7 +5,9 @@ function modify_qty(val) {
     if (new_qty < 0) {
         new_qty = 0;
     }
-    
+    else if (new_qty = qty) {
+        document.getElementById('qtf').innerHTML = 'Max';;
+    }
     document.getElementById('qty').value = new_qty;
     return new_qty;
 }
